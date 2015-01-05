90f82c79ed0ae84e4e09588eb42ed8023543f287
sara <shanian@gmail.com>
2015-01-05 10:55:14 -0500

    added but commented lines of code for getting the file name and story name in command line

--- src/main/java/com/impactanalysis/smart/TfIdfMain.java
+++ src/main/java/com/impactanalysis/smart/TfIdfMain.java
@@ -23,15 +23,40 @@ import org.xml.sax.SAXException;
     
     {
     	// this is an example for finding similar stories to EJS-163.
-    	
-        DocumentParser dp = new DocumentParser();
+	  
+    	String storiesFile="C:\\folderToIndex\\training.xml";
+    	String newStory="EJS-163";
+	  /*
+	   if (args.length >0) {
+		  
+		   File f = new File(args[0]);
+		   if(f.exists() && !f.isDirectory()){
+			   storiesFile=args[0];
+		   }
+		   else {
+			   System.out.println(args[0] + "is not a valid file");
+		   }
+		   		   
+		   newStory=args[1];
+		  
+		  
+		   
+	   }
+	   
+	   else if (args.length ==0) {
+		   System.out.println("stories file and new story to check is missing");
+	   }
+	      
+       */
+	   
+	   	DocumentParser dp = new DocumentParser();
         TfIdf tfidf=new TfIdf();
         CosineSimilarity cs= new CosineSimilarity();
         
         dp.extractJiraStories("C:\\folderToIndex\\training.xml");
         tfidf.tfIdfCalculator(dp.getJiraStoryNumbers(), dp.getAllTerms(), dp.getTokenizedjiraStory());
         cs.getCosineSimilarity("EJS-163", dp.getJiraStoryNumbers(), tfidf.getTfidfDocsVector());
-               
+	           
     }
       
 }
